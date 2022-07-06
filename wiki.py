ca1a757a5b45b4a7234eafe7da5281199e1cdfd5
Lívia Almeida <leave.ah@gmail.com>
2022-07-06 08:57:17 +0200

    Using build_vocab instead of get_texts

--- wiki.py
+++ wiki.py
@@ -1,25 +1,17 @@
 from sys import argv
 import multiprocessing
-from gensim.corpora import WikiCorpus
 from gensim.models import Word2Vec
-
-def getsentences(corpus):
-  sentences = []
-  for index, sentence in enumerate(corpus.get_texts()):
-    sentences.append(sentence)
-    if index % 10000 == 0:
-      print(f'Processed {index} texts')
-  print(f'Finished. Processed {len(sentences)} texts')
-  return sentences
+from gensim.models.word2vec import LineSentence
 
 def createmodel(sentences):
   workers = max(1, multiprocessing.cpu_count() - 1)
-  word2vec = Word2Vec(sentences, vector_size=200, window=10, min_count=10, workers=workers, sample=1E-3)
+  word2vec = Word2Vec(None, min_count=10, sample=1E-3, sg=1, window=10, workers=workers, vector_size=500)
+  word2vec.build_vocab(sentences)
+  word2vec.train(sentences)
   return word2vec
 
 if __name__ == "__main__":
   if len(argv) == 2:
-    wiki = WikiCorpus(argv[1], dictionary={}, processes=1, lower=True)
-    sentences = getsentences(wiki)
+    sentences = LineSentence(argv[1])
     word2vec = createmodel(sentences)
     word2vec.wv.save('wiki.wv')
